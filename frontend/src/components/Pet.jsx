e80d9ab00ed9a6fd6e995b02891b0744422ef012
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-09-30 09:10:14 -0400

    Added Pet component

---
+++ frontend/src/components/Pet.jsx
@@ -0,0 +1,12 @@
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>Page</h1>
+        <h2>
+        </h2>
+    </div>
+  )
+}
+
+export default Pet
--- frontend/src/components/Pets.jsx
+++ frontend/src/components/Pets.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Pet from './Pet'
 
 const Pets = (props) => {
 	return (
     <div className='pet-wrapper'>
 			Pets
+			<Pet/>
     </div>
   )
 }
