b35c2e5ff8b137eff997fe0fbe3f60c5c7cfcfab
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-09-30 10:05:02 -0400

    Fixed compiling bug

--- frontend/src/features/pets/petSlice.js
+++ frontend/src/features/pets/petSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from 'reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import petService from './petService'
 
 const initialState = {
