df71657b473393f7478f4a82ff940c317154e30f
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-09-30 09:42:41 -0400

    Created basic pet slice and service

--- frontend/src/features/pets/petService.js
+++ frontend/src/features/pets/petService.js
@@ -1,5 +1,22 @@
-const petService = {
+import axios from 'axios'
 
+const API_URL = '/api/pets/'
+
+// Get pets
+const getPets = async (token) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  
+    const response = await axios.get(API_URL, config)
+  
+    return response.data
+  }
+
+const petService = {
+    getPets,
 }
 
 export default petService
--- frontend/src/features/pets/petSlice.js
+++ frontend/src/features/pets/petSlice.js
@@ -1,21 +1,62 @@
 import { createSlice, createAsyncThunk } from 'reduxjs/toolkit'
+import petService from './petService'
 
 const initialState = {
-
+  pets: [],
+  isLoading: false,
 }
+
+// Get pets
+export const getPets = createAsyncThunk(
+  'pets',
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await petService.getPets(token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+// Create new pet
+export const createPet = createAsyncThunk(
+  'pet/create',
+  async (petData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await petService.createGoal(petData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
 
 export const petSlice = createSlice({
   name: 'pet',
   initialState,
   reducers: {
-
+    reset: (state) => initialState,
   },
   extraReducers: (builder) => {
-    builder.addCase(()=> {
-
-    })
+    builder
+      .addCase(getPets.pending, (state) => {
+        state.isLoading = true
+      })
   }
 })
 
-export const {} = petSlice.actions
+export const { reset } = petSlice.actions
 export default petSlice.reducer
