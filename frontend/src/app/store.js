f5827955a4af2114986e0e943cc7452c178ebd07
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-09-30 10:04:48 -0400

    Added reducer to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import petReducer from '../features/pets/petSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    pets: petReducer,
   },
 });
