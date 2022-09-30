fa315f0dfd41919aa403eb090803ba983b4ac1af
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-09-30 08:56:06 -0400

    Imported pics and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App () {
   return (
     <>
-    <Router>
-      <div>
-        <Routes>
-          <Route/>
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <Header/>
+        <div>
+          <Routes>
+            <Route/>
+          </Routes>
+        </div>
+      </Router>
+      <Home/>
     </>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header () {
+  return (
+    <div>
+      <img src="Pet1.jpg" className='img' alt='dog'></img>
+      <img src="Pet3.jpg" className='img' alt='pets'></img>
+      <img src="Pet6.jpg" className='img' alt='snake'></img>
+      <img src="Pet7.jpg" className='img' alt='hamster'></img>
+    </div>
+  )
+}
+
+export default Header
