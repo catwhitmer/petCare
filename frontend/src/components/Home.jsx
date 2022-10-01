97623c21cabcae935af11865be1470dad037fd75
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-01 12:51:37 -0400

    Refactored Home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-function Home () {
+const Home = () => {
   return (
     <div style={{justifyContent: 'center', width: '100%', position: 'absolute', paddingTop: '10px'}}>
       <h1 style={{color: '#28afb0'}}>
