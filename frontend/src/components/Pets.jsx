c8ea2d112e898e6e90c82dcf1bde2349f4c441c6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-03 09:37:05 -0400

    updated pets

--- frontend/src/components/Pets.jsx
+++ frontend/src/components/Pets.jsx
@@ -1,23 +1,19 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPets, reset } from '../features/pets/petSlice'
 import Pet from './Pet'
 
 const Pets = () => {
-  const navigate = useNavigate()
+
   const dispatch = useDispatch()
 
-  const pets = useSelector(state => state.pets.petSlice.pets)
-  const isLoading = useSelector(state => state.pets.petSlice.isLoading)
+  // const pets = useSelector(state => state.petSlice.pets)
+  // const isLoading = useSelector(state => state.petSlice.isLoading)
 
-  useEffect (() => {
-    if (isLoading === false) {
-      //dispatch(getPets())
-    console.log(pets)
-    console.log(isLoading)
-    }
-  }, [isLoading])
+
+    dispatch(getPets())
+    // console.log(isLoading)
+
   
 	return (
     <div className='pet-wrapper'>
