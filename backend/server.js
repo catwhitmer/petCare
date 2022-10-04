3e6c8e7743a4b1f44741a29d46c4bc48bb6c36c6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-04 09:47:01 -0400

    Removed cors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const cors = require('cors')
 const port = process.env.PORT || 5000
 
 connectDB()
@@ -11,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+
 
 app.use('/api/pets', require('./routes/petRoutes'))
 app.use('/api/todos', require('./routes/todoRoutes'))
