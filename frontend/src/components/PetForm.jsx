c7af1579b03bbc7fa8bbb56e04462dfde5514ce6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-05 16:30:00 -0400

    Updated pet form CSS

--- frontend/src/components/PetForm.jsx
+++ frontend/src/components/PetForm.jsx
@@ -24,38 +24,41 @@ function PetForm () {
 	}
 
 	return (
-		<form className='pet-wrapper' onSubmit={handleOnSubmit}>
-      <h1>Add A New Pet</h1>
-        <input 
-          type='text' 
-          name='name' 
-          value={name} 
-          placeholder= 'Name' 
-          onChange={handleOnChange}
-        />
-        <input 
-          type='text' 
-          name='species' 
-          value={'species'} 
-          placeholder= 'Species' 
-          onChange={handleOnChange} 
-        />
-        <input 
-          type='text' 
-          name='breed' 
-          value={'breed'} 
-          placeholder='Breed' 
-          onChange={handleOnChange}
-        />
-        <input 
-          type='text' 
-          name='age' 
-          value={'age'} 
-          placeholder= 'Age' 
-          onChange={handleOnChange} 
-        />
-        <button type='submit'>Submit</button>
-    </form>
+    <div className='pet-wrapper'>
+      <form className="pet-form" onSubmit={handleOnSubmit}>
+        <h1>Add A New Pet</h1>
+          <input 
+            type='text' 
+            name='name' 
+            value={'name'} 
+            placeholder= 'Name' 
+            onChange={handleOnChange}
+          />
+          <input 
+            type='text' 
+            name='species' 
+            value={'species'} 
+            placeholder= 'Species' 
+            onChange={handleOnChange} 
+          />
+          <input 
+            type='text' 
+            name='breed' 
+            value={'breed'} 
+            placeholder='Breed' 
+            onChange={handleOnChange}
+          />
+          <input 
+            type='text' 
+            name='age' 
+            value={'age'} 
+            placeholder= 'Age' 
+            onChange={handleOnChange} 
+          />
+          <button type='submit'>Submit</button>
+      </form>
+    </div>
+		
   )
 }
 
