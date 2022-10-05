9a11265ca005a91910b62a0c897624b57ace42b6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-05 10:25:43 -0400

    Added link to pet page

--- frontend/src/components/Pets.jsx
+++ frontend/src/components/Pets.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPets } from '../features/pets/petSlice'
+import { Link } from 'react-router-dom'
 
 const Pets = () => {
 
@@ -12,12 +13,13 @@ const Pets = () => {
     dispatch(getPets())
   }, [])
   
-  
 	return (
     <div className='pet-wrapper'>
       <h1>My Pets</h1>
       {petData.map((pet) => (
-        <h3>{pet.name}</h3>
+        <h3 key={pet._id}>
+          <Link to={`/pets/${pet._id}`}>{pet.name}</Link>
+        </h3> 
       ))}
     </div>
   )
