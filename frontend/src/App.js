3763d1d1a919ffbcd9c56b2e25c9bfe1ec74cde9
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-06 14:57:09 -0400

    Removed pet container and moved pets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import NavBar from './components/NavBar'
-import PetsContainer from './containers/PetsContainer'
+import Pets from './components/Pets'
 import Pet from './components/Pet'
 import PetForm from './components/PetForm'
 import Home from './components/Home'
@@ -14,7 +14,7 @@ function App () {
         <div>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/pets' element={<PetsContainer />} />
+            <Route path='/pets' element={<Pets />} />
             <Route path='/pets/new' element={<PetForm />} />
             <Route path='/pets/:id' element={<Pet />} />
           </Routes>
