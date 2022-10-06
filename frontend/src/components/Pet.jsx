9cc2be1f6a40a808287afc68314ab8ded910368c
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-06 15:16:17 -0400

    Updated CSS

--- frontend/src/components/Pet.jsx
+++ frontend/src/components/Pet.jsx
@@ -1,23 +1,33 @@
 import { useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Card from '@mui/material/Card'
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const Pet = () => {
 
   const location = useLocation()
+  const navigate = useNavigate()
 
   const petData = useSelector(state => state.pets.pets)
 
   const petID = location.pathname.replace('/pets/', '')
   const currentPet = petData.filter((pet) => pet._id === petID)
-  
+
   return (
-    <div className='pet-wrapper'>
-      <h1>{currentPet[0].name}</h1>
-      <div className='pet'>
-        <h3 style={{textAlign: 'left'}}>{currentPet[0].species}</h3>
-        <h3 style={{textAlign: 'left'}}>{currentPet[0].breed}</h3>
-        <h3 style={{textAlign: 'left'}}>{currentPet[0].age}</h3>
-      </div>
+    <div className='pet-wrapper'>    
+        <Card className='pet'>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <h1>{currentPet[0].name}</h1>
+            <AutoFixHighIcon className='icon' style={{paddingLeft: '40px'}} />
+            <DeleteForeverIcon className='icon' />
+          </div>
+          <div style={{textAlign: 'left'}}>
+            <h3>{currentPet[0].species}</h3>
+            <h3>{currentPet[0].breed}</h3>
+            <h3>{currentPet[0].age}</h3>
+          </div>
+        </Card>
     </div>
   )
 }
