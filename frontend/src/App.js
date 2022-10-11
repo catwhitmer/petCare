9491525fbe78c240a1724e11c7106a5774f6440c
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-11 09:22:08 -0400

    Changed route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App () {
             <Route path='/' element={<Home />} />
             <Route path='/pets' element={<Pets />} />
             <Route path='/pets/new' element={<PetForm />} />
-            <Route path='/pets/edit' element={<PetForm />}/>
+            <Route path='/pets/:id/edit' element={<PetForm />}/>
             <Route path='/pets/:id' element={<Pet />} />
           </Routes>
         </div>
