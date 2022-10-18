7a81018e5201bba2f31410bc6cb5f6326f64136a
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-18 11:47:17 -0400

    Updated todos

--- frontend/src/components/todos.jsx
+++ frontend/src/components/todos.jsx
@@ -3,13 +3,16 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { getTodos } from '../features/todos/todoSlice'
 
 const Todos = () => {
 
   const dispatch = useDispatch()
 
+  const todoData = useSelector(state => state.todos.todos)
+
   useEffect(() => {
-    //dispatch()
+    dispatch(getTodos())
   }, [])
 
   const handleTodoDelete = () => {
@@ -18,13 +21,17 @@ const Todos = () => {
 
   return (
     <Card className='card'>
-      <DeleteForeverIcon className='icon' onClick={handleTodoDelete} style={{float: 'right'}}/>
-        <h2>To-Dos</h2>
-          <div className="todo">
-            <li style={{float: 'left'}}>Go for a walk</li>
-            <DeleteForeverIcon className="todo-icon" style={{width: '18px'}}/>
-            <AutoFixHighIcon className="todo-icon"style={{width: '18px'}}/>
-          </div>
+      <h2>To-Dos</h2>
+        <div className="todo">
+          {todoData.map((todo) => ( 
+            <>    
+              <li>{todo.description}</li>
+              <li style={{float: 'left'}}>{todo.notes}</li>
+              <DeleteForeverIcon className="todo-icon" style={{width: '18px'}}/>
+              <AutoFixHighIcon className="todo-icon"style={{width: '18px'}}/>
+            </>
+          ))}
+        </div>
     </Card>
   )
 }
