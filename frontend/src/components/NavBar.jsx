fbc8fbab0c8c616ac4837695f19c72009c7fa0a0
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-18 09:08:25 -0400

    Updated nav bar CSS

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
-import Header from './Header'
 
 function NavBar () {
   return (
-      <div className='NavBar'>
-      <Link to='/'>Home</Link> - <Link to='/pets'>All Pets</Link> - <Link to='/pets/new'>Add Pet</Link>
-        <div style={{display: 'flex', float: 'right'}}>
-          <Header/>
+      <div className='nav-bar'>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link> - <Link to='/pets'>All Pets</Link> - <Link to='/pets/new'>Add Pet</Link>
+        </div>
+        <div className='nav-images'>
+          <img src="Pet1.jpg" className='img' alt='dog'></img>
+          <img src="Pet3.jpg" className='img' alt='pets'></img>
+          <img src="Pet6.jpg" className='img' alt='snake'></img>
+          <img src="Pet7.jpg" className='img' alt='hamster'></img>
         </div>
     </div>
   )
