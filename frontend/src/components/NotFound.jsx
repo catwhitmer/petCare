682aa184d86c3156f464a6ac206301d60346c168
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-20 11:07:40 -0400

    Updated Not Found page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+import HomeIcon from '@mui/icons-material/Home';
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <h1>
+        Oops!
+      </h1>
+      <p>
+        404 - Page Not Found!
+      </p>
+        <Link to='/' >
+          <HomeIcon />
+          Home
+        </Link>
+    </div>
   )
 }
 
