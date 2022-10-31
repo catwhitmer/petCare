c044f0b9e212752c259f28c8a66687208c78a8e2
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-31 10:47:04 -0400

    Fixed typo

--- frontend/src/components/Pet.jsx
+++ frontend/src/components/Pet.jsx
@@ -17,7 +17,7 @@ const Pet = () => {
   const currentPet = petData.filter((pet) => pet._id === petID)
 
   const handlePetDelete = () => {
-    if (window.confirm('Are you sur you want to delete this pet?')) {
+    if (window.confirm('Are you sure you want to delete this pet?')) {
       if (currentPet[0]._id === petID) {
         dispatch(deletePet(petID))
         navigate('/pets')
@@ -36,7 +36,7 @@ const Pet = () => {
             <h3>{currentPet[0].age}</h3>
           </div>
         </Card>
-        <Todos/>
+        <Todos />
     </div>
   )
 }
