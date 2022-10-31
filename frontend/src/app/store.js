f9d1273cd50dff63492ce44f57908f0d47bf5286
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-31 10:46:50 -0400

    Added todoReducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import petReducer from '../features/pets/petSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import petReducer from '../features/pets/petSlice'
+import todoReducer from '../features/todos/todoSlice'
 
 export const store = configureStore({
   reducer: {
     pets: petReducer,
+    todos: todoReducer
   },
 });
