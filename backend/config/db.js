4dbeeb969dd6efe4b8446be29d567d30f409c40c
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-11-10 16:40:24 -0500

    Removed console.log

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
     try {
         const conn = await  mongoose.connect(process.env.MONGO_URI)
 
-        console.log(`MongoDb Connected: ${conn.connection.host}`)
     } catch (error) {
         process.exit(1)
     }
