8f3cb1dcde5ccac800fa5dcdfaceb2b40b9dd617
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-11-11 10:12:25 -0500

    Updated pics in nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,10 +7,10 @@ function NavBar () {
           <Link to='/'>Home</Link> - <Link to='/pets'>All Pets</Link> - <Link to='/pets/new'>Add Pet</Link>
         </div>
         <div className='nav-images'>
-          <img src="Pet1.jpg" className='img' alt='dog'></img>
-          <img src="Pet3.jpg" className='img' alt='pets'></img>
-          <img src="Pet6.jpg" className='img' alt='snake'></img>
-          <img src="Pet7.jpg" className='img' alt='hamster'></img>
+          <img src="Pet1.png" className='img' alt='dog'></img>
+          <img src="Pet3.png" className='img' alt='pets'></img>
+          <img src="Pet6.png" className='img' alt='snake'></img>
+          <img src="Pet7.png" className='img' alt='hamster'></img>
         </div>
     </div>
   )
