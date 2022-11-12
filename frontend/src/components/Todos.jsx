84e8b98630e2a287e74ceff691535241eebefbdd
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-11-12 14:07:45 -0500

    Bug fix

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,9 +1,9 @@
 import { Card } from "@mui/material"
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getTodos, updateTodo, deleteTodo } from '../features/todos/todoSlice'
+import { getTodos, deleteTodo } from '../features/todos/todoSlice'
+
 
 const Todos = ({ currentPet }) => {
 
@@ -14,17 +14,12 @@ const Todos = ({ currentPet }) => {
 
   useEffect(() => {
     dispatch(getTodos())
-  }, [todos])
+  }, [])
 
   const handleDelete = (todo) => {
     if (window.confirm('Are you sure you want to delete this todo?')) {
       dispatch(deleteTodo(`${todo._id}`))
     }
-  }
-
-  const handleUpdate = (todo) => {
-    console.log('clicked')
-    // dispatch(updateTodo(`${todo._id}`))
   }
 
   return (
@@ -33,7 +28,6 @@ const Todos = ({ currentPet }) => {
         {todos.map((todo) => ( 
           <div className="todo" key={todo._id}>  
             <DeleteForeverIcon className="todo-icon" onClick={() => handleDelete(todo)} style={{width: '18px'}}/>
-            <AutoFixHighIcon className="todo-icon" onClick={() => handleUpdate(todo)} style={{width: '18px'}}/>
             <p >{todo.description} - {todo.notes}</p>
           </div>
         ))}
